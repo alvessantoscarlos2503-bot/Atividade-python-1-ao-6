7ea28b8fd505b273bf2a3e5529b127f6aab18d28
carlosgpuava <alves.santos.carlos2503@escola.pr.gov.br>
2026-04-15 11:58:04 -0300

    Add files via upload

---
+++ Atividade Python Carlos/ex09.py
@@ -0,0 +1,13 @@
+# Verificando se um número é positivo ou negativo 
+numero = int(input("Digite um número"))
+
+# Criando uma condição
+if numero > 0:
+    print("Número positivo")
+
+elif numero == 0:
+    
+    print("Número é zero")
+
+else: 
+     print("numero negativo")
---
+++ Atividade Python Carlos/ex11.py
@@ -0,0 +1,8 @@
+# Número par ou ímpar
+numero = int (input("Digite um número:"))
+
+if numero % 2 == 0:
+ print(" Número par")
+
+else: 
+  print("Número ímapar")
