34442ca163e6eb5e9026b260690bba1b2ba92812
carlosgpuava <alves.santos.carlos2503@escola.pr.gov.br>
2026-04-22 16:19:53 -0300

    Add files via upload

---
+++ Nova pasta/ex24.py
@@ -0,0 +1,8 @@
+# Total do pedido
+produto = input("Digite o nome do produto: ")
+preco = float(input("Digite o preço do produto: R$ "))
+quantidade = int(input("Digite a quantidade do produto: "))
+
+print(f"Produto: {produto}")
+print(f"Preço: R$ {preco:.2f}")
+print(f"Quantidade: {quantidade}")
---
+++ Nova pasta/ex25.py
@@ -0,0 +1,17 @@
+# Desconto do pedido
+produto = input("Digite o nome do produto: ")
+preco = float(input("Digite o preço do produto: R$ "))
+quantidade = int(input("Digite a quantidade do produto: "))
+
+total = preco * quantidade
+
+if total > 50: 
+    desconto = total * 0.10
+    total_com_desconto = total - desconto
+    print("Desconto aplicado: 10%")
+else:
+    total_final = total
+    print("Sem desconto")
+
+print(f"Produto: {produto}")
+print(f"Total a pagar: R$ { total_final:.2f}")
