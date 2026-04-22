326cb75d97524725508d37f7b72068f19a30ee1f
carlosgpuava <alves.santos.carlos2503@escola.pr.gov.br>
2026-04-22 15:43:41 -0300

    Delete Atividade Python Carlos directory

--- Atividade Python Carlos/ex08.py
+++
@@ -1,6 +0,0 @@
-age = int (input("Digite sua idade: "))
-
-if age >= 18:
-    print("Você é maior de idade")   
-else:
-    print("Você é menor de idade")  
--- Atividade Python Carlos/ex11.py
+++
@@ -1,8 +0,0 @@
-# Número par ou ímpar
-numero = int (input("Digite um número:"))
-
-if numero % 2 == 0:
- print(" Número par")
-
-else: 
-  print("Número ímapar")
